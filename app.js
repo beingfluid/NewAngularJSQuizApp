7cf5058ade0227e1a70381160632b415d58fbc35
Vishal Ingle <50449372+beingfluid@users.noreply.github.com>
2023-01-18 01:18:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,28 @@ app.controller('QuizCtrl', function($scope, $http) {
     $scope.questions = response.data;
   });
   
-  $scope.submit = function() {
+ $scope.submit = function() {
     var correctAnswers = 0;
     angular.forEach($scope.questions, function(question) {
-      if (question.type === 'truefalse') {
-        if (question.answer === question.userAnswer) {
-          correctAnswers++;
+        if (question.type === 'truefalse') {
+            if (question.answer === question.userAnswer) {
+                correctAnswers++;
+            }
+        } else if (question.type === 'multiplechoice') {
+            if (question.answer === question.userAnswer) {
+                correctAnswers++;
+            }
+        } else if (question.type === 'multiplecorrect') {
+            if(Array.isArray(question.userAnswer)){
+                var userAnswers = question.userAnswer.sort();
+                var correctAnswers = question.answer.sort();
+                if (angular.equals(userAnswers, correctAnswers)) {
+                    correctAnswers++;
+                }
+            }
         }
-      } else if (question.type === 'multiplechoice') {
-        if (question.answer === question.userAnswer) {
-          correctAnswers++;
-        }
-      } else if (question.type === 'multiplecorrect') {
-        var userAnswers = question.userAnswer.sort();
-        var correctAnswers = question.answer.sort();
-        if (angular.equals(userAnswers, correctAnswers)) {
-          correctAnswers++;
-        }
-      }
     });
     $scope.score = correctAnswers;
-  };
+};
+
 });
